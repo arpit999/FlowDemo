0b7fefb0d06b8a4e77a33f5bbdfd391d6535e1ef
Arpit Patel <arpit@temi.ca>
2022-09-18 16:29:43 -0400

    Cancel flow using cancelling coroutine. There is no specific method to cancel flow.

--- app/src/main/java/com/hometech/flowdemo/MainActivity.kt
+++ app/src/main/java/com/hometech/flowdemo/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
@@ -16,6 +17,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.hometech.flowdemo.ui.theme.FlowDemoTheme
 import kotlinx.coroutines.*
 import kotlinx.coroutines.flow.Flow
@@ -33,9 +35,6 @@ class MainActivity : ComponentActivity() {
             var users by remember {
                 mutableStateOf("")
             }
-            var delayCollect by remember {
-                mutableStateOf("")
-            }
             FlowDemoTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
@@ -45,27 +44,26 @@ class MainActivity : ComponentActivity() {
                         horizontalAlignment = Alignment.CenterHorizontally
                     ) {
 
-                        Text(text = "After 3 Seconds \n$delayCollect")
-
                         Button(modifier = Modifier, onClick = {
-                           coroutineScope.launch(Dispatchers.Main) {
+                            coroutineScope.launch(Dispatchers.Main) {
                                 data.collect {
                                     Log.d("UI", "Normal Collect: $it")
                                     users = users.plus("User $it").plus("\n")
                                 }
                             }
 
-                            coroutineScope.launch(Dispatchers.Main) {
-                                // Collect each emission after 3 second and getting all  emissions from start to end
-                                delay(3000)
-                                data.collect {
-                                    Log.d("delayCollect", "Delay Collect: $it")
-                                    delayCollect = delayCollect.plus("User $it").plus("\n")
-                                }
-                            }
-
                         }) {
                             Text(text = "Fire Flow", color = Color.White)
+                        }
+                        Button(modifier = Modifier.padding(top = 10.dp), onClick = {
+                            /**
+                             * Flow can be cancelled when coroutine is cancelled. If there is no collector then it will automatically be cancelled.
+                             * So here I am using coroutine job object to cancel the flow.
+                             * Here I am using coroutineContext to cancel the coroutine children. Because if I use coroutineScope.Cancel() then I can cancel that coroutine but it will not be start again.
+                             */
+                            coroutineScope.coroutineContext.cancelChildren()
+                        }) {
+                            Text(text = "Cancel Flow", color = Color.White)
                         }
                         Text(text = users, color = Color.DarkGray)
 
@@ -80,10 +78,9 @@ class MainActivity : ComponentActivity() {
  * This is simple example for cold flow. Cold flow never loose data if consumer join in middle of emissions than it will provide emission from start. It is advantage and disadvantage of cold flow.
  * If we would like to show all events to user in UI that it will make sense that we show all the event when user are not in that screen. But if we just need to show latest emission then  we can use Hot flow like Channel or SharedFlow.
  *
- * Here you can see that in UI we are starting second collector after 3 seconds and it is getting all the emissions from start to finish.
  */
 fun produceUser() = flow<Int> {
-    val list = listOf(1, 2, 3, 4, 5, 6)
+    val list = listOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14)
 
     list.forEach {
         delay(1000)

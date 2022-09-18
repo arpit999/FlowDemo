a473d52798abdb64f6a4f98ee9655ed409e4e873
Arpit Patel <arpit@temi.ca>
2022-09-18 16:03:54 -0400

    Adding multiple collectors. Even though second collector start after 3 seconds it is getting all emissions from start.

--- app/src/main/java/com/hometech/flowdemo/MainActivity.kt
+++ app/src/main/java/com/hometech/flowdemo/MainActivity.kt
@@ -7,7 +7,6 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.size
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
@@ -17,16 +16,10 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import com.hometech.flowdemo.ui.theme.FlowDemoTheme
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.currentCoroutineContext
-import kotlinx.coroutines.delay
+import kotlinx.coroutines.*
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
-import kotlinx.coroutines.flow.forEach
-import kotlinx.coroutines.launch
-import kotlin.coroutines.coroutineContext
 
 class MainActivity : ComponentActivity() {
     //declare consumer
@@ -40,6 +33,9 @@ class MainActivity : ComponentActivity() {
             var users by remember {
                 mutableStateOf("")
             }
+            var delayCollect by remember {
+                mutableStateOf("")
+            }
             FlowDemoTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
@@ -48,14 +44,26 @@ class MainActivity : ComponentActivity() {
                         verticalArrangement = Arrangement.Center,
                         horizontalAlignment = Alignment.CenterHorizontally
                     ) {
+
+                        Text(text = "After 3 Seconds \n$delayCollect")
+
                         Button(modifier = Modifier, onClick = {
-                            coroutineScope.launch(Dispatchers.Main) {
-                                // Collect each emission after 1 second
+                           coroutineScope.launch(Dispatchers.Main) {
                                 data.collect {
-                                    Log.d("UI", "onCreate: $it")
+                                    Log.d("UI", "Normal Collect: $it")
                                     users = users.plus("User $it").plus("\n")
                                 }
                             }
+
+                            coroutineScope.launch(Dispatchers.Main) {
+                                // Collect each emission after 3 second and getting all  emissions from start to end
+                                delay(3000)
+                                data.collect {
+                                    Log.d("delayCollect", "Delay Collect: $it")
+                                    delayCollect = delayCollect.plus("User $it").plus("\n")
+                                }
+                            }
+
                         }) {
                             Text(text = "Fire Flow", color = Color.White)
                         }
@@ -71,8 +79,9 @@ class MainActivity : ComponentActivity() {
 /**
  * This is simple example for cold flow. Cold flow never loose data if consumer join in middle of emissions than it will provide emission from start. It is advantage and disadvantage of cold flow.
  * If we would like to show all events to user in UI that it will make sense that we show all the event when user are not in that screen. But if we just need to show latest emission then  we can use Hot flow like Channel or SharedFlow.
+ *
+ * Here you can see that in UI we are starting second collector after 3 seconds and it is getting all the emissions from start to finish.
  */
-
 fun produceUser() = flow<Int> {
     val list = listOf(1, 2, 3, 4, 5, 6)
 
